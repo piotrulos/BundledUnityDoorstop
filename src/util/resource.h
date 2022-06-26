01c98efb15a80576b9ca83fcaf95c093e27116b3
某10 <0w0@0w0.pw>
2022-06-26 16:33:53 +0800

    nix missing util.h

--- src/util/resource.h
+++ src/util/resource.h
@@ -1,6 +1,9 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include "util.h"
+#include "../crt.h"
+
 bool_t resource_exists(const char *filename);
 void *resource_get(const char *filename);
 size_t resource_size(const char *filename);
